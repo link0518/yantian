0ae271a1b18efd2013982b82f3a847908ff99ca9
link0518 <104314575+link0518@users.noreply.github.com>
2024-09-24 22:25:20 +0800

    3

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,9 +1,9 @@
 export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/github/yantian/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
   ["/child/about.html", { loader: () => import(/* webpackChunkName: "child_about.html" */"D:/github/yantian/docs/.vuepress/.temp/pages/child/about.html.js"), meta: {"title":"了解我"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/github/yantian/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/github/yantian/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
 ]);
 
 if (import.meta.webpackHot) {
