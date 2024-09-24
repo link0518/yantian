156b884449586dd7cb9713177b6f7de521edcb55
link0518 <104314575+link0518@users.noreply.github.com>
2024-09-25 04:31:45 +0800

    修改样式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,11 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
+  base: "/yantian/",
   bundler: viteBundler(),
   theme: defaultTheme({
     title: 'My VuePress Site',
-    base: "/yantian/",
+    
     logo: '23.svg', //文件在public文件夹内
     nav: [
       { text: '你的文字', link: '/' }, // 在 logo 旁边添加文字
